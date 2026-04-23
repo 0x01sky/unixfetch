2933b3766509578268b1d28d24360e5ea2f23482
0x01sky <sky@git.com>
2026-04-23 19:34:55 +0100

    Updated

--- setup.sh
+++ setup.sh
@@ -2,26 +2,11 @@
 
 set -euo pipefail
 
-SHELL_TYPE=$(basename $SHELL)
 setup(){
-  chmod +x unixfetch.sh
-  mv unixfetch.sh ~/.local/bin/unixfetch
-  echo "(+) Setup is Completed! Now you can use unixfetch!"
+  chmod +x unixfetch
+  mv unixfetch /usr/bin/unixfetch
+  echo "Your setup is done (+)"
 }
 
-shell_type(){
-  if $SHELL_TYPE == "fish"; then
-    echo "set -gx $PATH $HOME/.local/bin $PATH" >> ~/.config/fish/config.fish
-    source ~/.config/fish/config.fish
-    setup
-  else
-    setup
-  fi
-}
-
-if [ ! -f ~/.local/bin ]; then
-  mkdir -p ~/.local/bin/
-  shell_type
-else
-  shell_type
+setup
 
